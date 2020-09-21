003620bd32179b7ab01d05e7fe72c5e9c8a35769
Alexander Oberländer <oberlaender@plt-software.de>
2020-09-21 14:06:21 +0200

    Remove non-working extension methods in favor of base class methods

--- src/SharperBunny/Interfaces/IConsumerBase.cs
+++ src/SharperBunny/Interfaces/IConsumerBase.cs
@@ -1,5 +1,11 @@
 namespace SharperBunny.Interfaces {
   public interface IConsumerBase {
-    
+    public IConsumerBase AsAutoAck(bool autoAck = true);
+
+    public IConsumerBase AddTag(string tag, object value);
+
+    public IConsumerBase UseUniqueChannel(bool useUnique = true);
+
+    public IConsumerBase Prefetch(ushort prefetchCount = 50);
   }
 }
